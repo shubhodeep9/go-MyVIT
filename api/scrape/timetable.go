33034bb4edb7d770b8ebdb113666acb97fb21a30
Shubhodeep Mukherjee <shubhodeep9@gmail.com>
2016-02-29 22:27:03 +0530

    Bug

--- api/scrape/timetable.go
+++ api/scrape/timetable.go
@@ -5,7 +5,6 @@ import (
 	"strings"
 	"go-MyVIT/api/Godeps/_workspace/src/github.com/headzoo/surf/browser"
 	"go-MyVIT/api/login"
-	"fmt"
 )
 
 type Timetable struct {
@@ -35,36 +34,39 @@ func ShowTimetable(bow *browser.Browser,regno, password string) *Timetable {
 	tables := bow.Find("table")
 	reg_table := tables.Eq(1)
 	conts := make(map[string]Contents)
-	reg_table.Find("tr").Each(func(i int, s *goquery.Selection){
-		if i>0 && i<reg_table.Find("tr").Length()-1 {
-			code := s.Find("td").Eq(3).Text()
+	tr := reg_table.Find("tr")
+	tr_len := tr.Length()
+	tr.Each(func(i int, s *goquery.Selection){
+		if i>0 && i<tr_len-2 {
+			td := s.Find("td")
+			code := td.Eq(3).Text()
 			if code == "Embedded Lab" {
-				code = s.Find("td").Eq(1).Text()+"_L"
+				code = td.Eq(1).Text()+"_L"
 				conts[code] = Contents{
-					Class_number: s.Find("td").Eq(0).Text(),
-					Course_code:s.Find("td").Eq(1).Text(),
-					Course_mode:s.Find("td").Eq(5).Text(),
-					Course_option:s.Find("td").Eq(6).Text(),
-					Course_title:s.Find("td").Eq(2).Text(),
-					Course_type:s.Find("td").Eq(3).Text(),
-					Faculty:s.Find("td").Eq(9).Text(),
-					Ltpjc:strings.TrimSpace(s.Find("td").Eq(4).Text()),
-					Slot:s.Find("td").Eq(7).Text(),
-					Venue:s.Find("td").Eq(8).Text(),
+					Class_number: td.Eq(0).Text(),
+					Course_code:td.Eq(1).Text(),
+					Course_mode:td.Eq(5).Text(),
+					Course_option:td.Eq(6).Text(),
+					Course_title:td.Eq(2).Text(),
+					Course_type:td.Eq(3).Text(),
+					Faculty:td.Eq(9).Text(),
+					Ltpjc:strings.TrimSpace(td.Eq(4).Text()),
+					Slot:td.Eq(7).Text(),
+					Venue:td.Eq(8).Text(),
 				}
 			} else {
 				conts[code] = Contents{
-					Class_number: s.Find("td").Eq(2).Text(),
-					Course_code:s.Find("td").Eq(3).Text(),
-					Course_mode:s.Find("td").Eq(7).Text(),
-					Course_option:s.Find("td").Eq(8).Text(),
-					Course_title:s.Find("td").Eq(4).Text(),
-					Course_type:s.Find("td").Eq(5).Text(),
-					Faculty:s.Find("td").Eq(11).Text(),
-					Ltpjc:strings.TrimSpace(s.Find("td").Eq(6).Text()),
-					Registration_status:s.Find("td").Eq(12).Text(),
-					Slot:s.Find("td").Eq(9).Text(),
-					Venue:s.Find("td").Eq(10).Text(),
+					Class_number: td.Eq(2).Text(),
+					Course_code:td.Eq(3).Text(),
+					Course_mode:td.Eq(7).Text(),
+					Course_option:td.Eq(8).Text(),
+					Course_title:td.Eq(4).Text(),
+					Course_type:td.Eq(5).Text(),
+					Faculty:td.Eq(11).Text(),
+					Ltpjc:strings.TrimSpace(td.Eq(6).Text()),
+					Registration_status:td.Eq(12).Text(),
+					Slot:td.Eq(9).Text(),
+					Venue:td.Eq(10).Text(),
 				}
 			}
 		}
