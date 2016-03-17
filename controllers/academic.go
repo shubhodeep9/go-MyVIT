5b25de65f9066960ae2c954f47eaef4ad15c3a13
Shubhodeep Mukherjee <shubhodeep9@gmail.com>
2016-03-17 22:32:34 +0530

    Academic History

---
+++ controllers/academic.go
@@ -0,0 +1,44 @@
+/*
+@Author Shubhodeep Mukherjee
+@Organization Google Developers Group VIT Vellore
+	Isn't Go sexy?
+	I know right!!
+	Just like Shubhodeep
+	I mean, have you seen the guy? xP
+	#GDGSwag
+*/
+
+
+package controllers
+
+import (
+	"github.com/astaxie/beego"
+	"go-MyVIT/api"
+)
+
+// Operations about login
+type AdvisorController struct {
+	beego.Controller
+}
+
+// @Title Get
+// @Description find object by objectid
+// @Success 200
+// @Failure 403 parameters missing
+// @router / [get]
+func (o *AdvisorController) Get() {
+	regNo := o.Input().Get("regNo")
+	psswd := o.Input().Get("psswd")
+	campus := o.Ctx.Input.Param(":campus")
+	var baseuri string
+	if campus == "vellore" {
+		baseuri = "https://academics.vit.ac.in"
+	} else {
+		baseuri = "https://academicscc.vit.ac.in"
+	}
+	if regNo != "" && psswd != "" {
+		resp := api.AcademicHistory(regNo, psswd, baseuri)
+		o.Data["json"] = resp
+	}
+	o.ServeJSON()
+}
