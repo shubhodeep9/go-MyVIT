b7aeda8359ac20bc395ef07ca02627b25f694702
Shubhodeep Mukherjee <shubhodeep9@gmail.com>
2016-07-04 12:52:59 +0530

    Current Users

--- api/cache/set.go
+++ api/cache/set.go
@@ -1,6 +1,7 @@
 package cacheSession
 
 import (
+	"fmt"
 	"github.com/patrickmn/go-cache"
 	"go-MyVIT/api/Godeps/_workspace/src/github.com/headzoo/surf/browser"
 	"net/http"
@@ -17,5 +18,6 @@ func SetSession(bow *browser.Browser, cac *cache.Cache, regno string) bool {
 		cachevalue := cacheval.(*MemCache)
 		bow.SetSiteCookies(cachevalue.MemCookie)
 	}
+	fmt.Println(cac.Items())
 	return found
 }
