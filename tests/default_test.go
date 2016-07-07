df392ff766cabd2a76a21917861fd5c202d93a5d
Shubhodeep Mukherjee <shubhodeep9@gmail.com>
2016-07-07 18:58:58 +0530

    Tests added

--- tests/default_test.go
+++ tests/default_test.go
@@ -67,3 +67,20 @@ func TestSpotlight(t *testing.T) {
 		})
 	})
 }
+
+func TestStats(t *testing.T) {
+	r, _ := http.NewRequest("GET", "/admin/stats", nil)
+	w := httptest.NewRecorder()
+	beego.BeeApp.Handlers.ServeHTTP(w, r)
+
+	beego.Trace("testing", "TestGet", "Code[%d]\n%s", w.Code, w.Body.String())
+
+	Convey("Subject: Test Station Endpoint\n", t, func() {
+		Convey("Status Code Should Be 200", func() {
+			So(w.Code, ShouldEqual, 200)
+		})
+		Convey("The Result Should Not Be Empty", func() {
+			So(w.Body.Len(), ShouldBeGreaterThan, 0)
+		})
+	})
+}
