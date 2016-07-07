54e84bf8dd3eeba43ea2338dc52d0adef304bd4a
Shubhodeep Mukherjee <shubhodeep9@gmail.com>
2016-07-07 18:56:18 +0530

    Tests added

--- tests/default_test.go
+++ tests/default_test.go
@@ -1,15 +1,14 @@
 package test
 
 import (
+	"github.com/astaxie/beego"
+	. "github.com/smartystreets/goconvey/convey"
 	_ "go-MyVIT/routers"
 	"net/http"
 	"net/http/httptest"
 	"path/filepath"
 	"runtime"
 	"testing"
-
-	"github.com/astaxie/beego"
-	. "github.com/smartystreets/goconvey/convey"
 )
 
 func init() {
@@ -18,9 +17,42 @@ func init() {
 	beego.TestBeegoInit(apppath)
 }
 
-// TestGet is a sample to run an endpoint test
-func TestGet(t *testing.T) {
-	r, _ := http.NewRequest("POST", "/campus/vellore/login", nil)
+func TestLogin(t *testing.T) {
+	r, _ := http.NewRequest("POST", "/campus/vellore/login?regNo=14ABC0002&psswd=234", nil)
+	w := httptest.NewRecorder()
+	beego.BeeApp.Handlers.ServeHTTP(w, r)
+
+	beego.Trace("testing", "TestGet", "Code[%d]\n%s", w.Code, w.Body.String())
+
+	Convey("Subject: Test Station Endpoint\n", t, func() {
+		Convey("Status Code Should Be 200", func() {
+			So(w.Code, ShouldEqual, 200)
+		})
+		Convey("The Result Should Not Be Empty", func() {
+			So(w.Body.Len(), ShouldBeGreaterThan, 0)
+		})
+	})
+}
+
+func TestRefresh(t *testing.T) {
+	r, _ := http.NewRequest("POST", "/campus/vellore/refresh?regNo=14ABC0002&psswd=234", nil)
+	w := httptest.NewRecorder()
+	beego.BeeApp.Handlers.ServeHTTP(w, r)
+
+	beego.Trace("testing", "TestGet", "Code[%d]\n%s", w.Code, w.Body.String())
+
+	Convey("Subject: Test Station Endpoint\n", t, func() {
+		Convey("Status Code Should Be 200", func() {
+			So(w.Code, ShouldEqual, 200)
+		})
+		Convey("The Result Should Not Be Empty", func() {
+			So(w.Body.Len(), ShouldBeGreaterThan, 0)
+		})
+	})
+}
+
+func TestSpotlight(t *testing.T) {
+	r, _ := http.NewRequest("GET", "/campus/vellore/spotlight", nil)
 	w := httptest.NewRecorder()
 	beego.BeeApp.Handlers.ServeHTTP(w, r)
 
