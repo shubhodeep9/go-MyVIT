a856aa75e8ad4dcfbe92114b075e583a1af1eccc
Shubhodeep Mukherjee <shubhodeep9@gmail.com>
2016-07-13 16:29:29 +0530

    Cent percent

--- api/login/parse.py
+++
@@ -1,9 +0,0 @@
-from CaptchaParser import CaptchaParser
-from PIL import Image
-import sys
-reg = sys.argv[1]
-img = Image.open("api/login/"+reg+".bmp")
-parser = CaptchaParser()
-captcha = parser.getCaptcha(img)
-
-print captcha
