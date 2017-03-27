f327e748df01b04190fb2fe17a95e2eeaf6b5c7e
Ujjwal Ayyangar <ujjwal.097@gmail.com>
2017-03-27 19:26:07 +0530

    Update examSchedule.go

--- api/scrape/examSchedule.go
+++ api/scrape/examSchedule.go
@@ -9,18 +9,32 @@ package scrape
 import (
 	"go-MyVIT/api/Godeps/_workspace/src/github.com/PuerkitoBio/goquery"
 	"go-MyVIT/api/Godeps/_workspace/src/github.com/headzoo/surf/browser"
-	"os"
-	"sync"
+	"go-MyVIT/api/status"
+	//"os"
+	//"sync"
 )
 
 type ExamSchedule struct {
-	Status  string               `json:"status"`
-	Cat1    map[string]Contents2 `json:"cat1"`
-	Cat2    map[string]Contents2 `json:"cat2"`
-	TermEnd map[string]Contents2 `json:"termend"`
+	Status  string              `json:"status"`
+	Cat1    map[string]Content3 `json:"cat1"`
+	Cat2    map[string]Content3 `json:"cat2"`
+	TermEnd map[string]Content3 `json:"termend"`
+}
+type MainExamSchedule struct {
+	Exam_Schedule FinalExamSchedule `json:"exam_schedule"`
 }
 
-type Contents2 struct {
+type FinalExamSchedule struct {
+	Status status.StatusStruct `json:"status"`
+	Exams  []exms              `json:"exams"`
+}
+type exms struct {
+	Name     string     `json:"name"`
+	Schedule []Content3 `json:"schedule"`
+}
+
+type Content3 struct {
+	CourseCode   string `json:'courseCode'`
 	Course_Title string `json:"crTitle"`
 	Slot         string `json:"slot"`
 	Date         string `json:"date"`
@@ -34,55 +48,63 @@ Function ->ExmSchedule to fetch the exam schedule,
 @return ExamSchedule struct
 */
 
-func ExmSchedule(bow *browser.Browser, baseuri string) *ExamSchedule {
-	sem := os.Getenv("SEM")
-	status := "Success"
-	var cat1 map[string]Contents2
-	var cat2 map[string]Contents2
-	var fat map[string]Contents2
-	list := make([]map[string]Contents2, 100)
+func ExmSchedule(bow *browser.Browser, reg, baseuri string, found bool) *MainExamSchedule {
+	result := []exms{}
+	var b FinalExamSchedule
+
+	//sem := os.Getenv("SEM")
+	//sem = "WS"
+	stat := status.Success()
+	var cat1 map[string]Content3
+	var cat2 map[string]Content3
+	var fat map[string]Content3
+	list := make([]map[string]Content3, 100)
 	list_count := 0
 	count := 0
-	if false {
-		status = "Failure"
+	if !found {
+		stat = status.SessionError()
 	} else {
-		var wg sync.WaitGroup
-		bow.Open(baseuri + "/student/exam_schedule.asp?sem=" + sem)
+		//var wg sync.WaitGroup
+		//student/exam_schedule.asp?
+		//exam_schedule.asp?sem=WS
+
+		bow.Open(baseuri + "/student/exam_schedule.asp?sem=WS")
 		//Reload
-		if bow.Open(baseuri+"/student/exam_schedule.asp?sem="+sem) == nil {
+		if bow.Open("https://vtop.vit.ac.in/student/exam_schedule.asp?sem=WS") == nil {
 			table := bow.Find("table").Eq(1)
 			rows := table.Find("tr").Length()
-			dets := make(map[string]Contents2)
+			dets := make(map[string]Content3)
 
 			table.Find("tr").Each(func(i int, s *goquery.Selection) {
 				if i >= 1 && i < rows-1 {
-					wg.Add(1)
-					go func() {
-						defer wg.Done()
-						td := s.Find("td")
+					//wg.Add(1)
+					//go func() {
+					//defer wg.Done()
+					td := s.Find("td")
 
-						if td.Length() == 1 {
-							// dets is a map of type signature - keyType = String and valueType = Contents2. ["Code":Contents2,"Code":Contents2]
-							// dets map will be reset for every examType that is why it is being intialized again and again
-							if count > 0 {
-								list_count = list_count + 1
-								dets = make(map[string]Contents2)
-							}
-							count = count + 1
-						} else {
-							dets[td.Eq(1).Text()] = Contents2{
-								Course_Title: td.Eq(2).Text(),
-								Slot:         td.Eq(4).Text(),
-								Date:         td.Eq(5).Text(),
-								Day:          td.Eq(6).Text(),
-								Session:      td.Eq(7).Text(),
-								Time:         td.Eq(8).Text()}
-							list[list_count] = dets
+					if td.Length() == 1 {
+						// dets is a map of type signature - keyType = String and valueType = Contents2. ["Code":Contents2,"Code":Contents2]
+						// dets map will be reset for every examType that is why it is being intialized again and again
+						if count > 0 {
+							list_count = list_count + 1
+							dets = make(map[string]Content3)
 						}
-					}()
+						count = count + 1
+					} else {
+						dets[td.Eq(1).Text()] = Content3{
+							CourseCode:   td.Eq(1).Text(),
+							Course_Title: td.Eq(2).Text(),
+							Slot:         td.Eq(4).Text(),
+							Date:         td.Eq(5).Text(),
+							Day:          td.Eq(6).Text(),
+							Session:      td.Eq(7).Text(),
+							Time:         td.Eq(8).Text()}
+						list[list_count] = dets
+					}
+					//}()
 				}
 			})
-			wg.Wait()
+			//wg.Wait()
 
 			if len(list[0]) != 0 {
 				cat1 = list[0]
@@ -94,9 +116,43 @@ func ExmSchedule(bow *browser.Browser, baseuri string) *ExamSchedule {
 				fat = list[2]
 			}
 			if len(list) == 0 {
-				status = "Failure"
+				stat = status.SessionError()
 			}
+			//sched1 := []Content3{}
+
+			sched1 := []Content3{}
+			for _, v := range cat1 {
+				sched1 = append(sched1, v)
+			}
+			exm1 := exms{
+				Name:     "CAT 1",
+				Schedule: sched1,
+			}
+			sched2 := []Content3{}
+			for _, v := range cat2 {
+				sched2 = append(sched2, v)
+			}
+			exm2 := exms{
+				Name:     "CAT 2",
+				Schedule: sched2,
+			}
+			sched3 := []Content3{}
+			for _, v := range fat {
+				sched3 = append(sched3, v)
+			}
+			exm3 := exms{
+				Name:     "FAT",
+				Schedule: sched3,
+			}
+			result = append(result, exm1, exm2, exm3)
+			b = FinalExamSchedule{
+				Status: stat,
+				Exams:  result,
+			}
+
 		}
 	}
-	return &ExamSchedule{Status: status, Cat1: cat1, Cat2: cat2, TermEnd: fat}
+
+	return &MainExamSchedule{Exam_Schedule: b}
+
 }
