4ab7a999bc6726c0e73f516707cae30581a2d16a
Ujjwal Ayyangar <ujjwal.097@gmail.com>
2017-04-16 22:53:33 +0530

    timetable2 controller

---
+++ controllers/timetable2.go
@@ -0,0 +1,39 @@
+/*
+timetable2 controller
+*/
+
+package controllers
+
+import (
+	//"fmt"
+	"github.com/astaxie/beego"
+	"go-MyVIT/api"
+)
+
+// Operations about login
+type Timetable2Controller struct {
+	beego.Controller
+}
+
+// @Title Get
+// @Description find object by objectid
+// @Success 200
+// @Failure 403 parameters missing
+// @router / [post]
+func (o *Timetable2Controller) Post() {
+	//fmt.Println("controller")
+	regNo := o.Input().Get("regNo")
+	psswd := o.Input().Get("psswd")
+	campus := o.Ctx.Input.Param(":campus")
+	var baseuri string
+	if campus == "vellore" {
+		baseuri = "https://vtop.vit.ac.in"
+	} else {
+		baseuri = "https://academicscc.vit.ac.in"
+	}
+	if regNo != "" && psswd != "" {
+		resp := api.ShowTimetable2(regNo, psswd, baseuri)
+		o.Data["json"] = resp
+	}
+	o.ServeJSON()
+}
